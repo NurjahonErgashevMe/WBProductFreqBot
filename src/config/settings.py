aafa1c771da8cd20277cadf7092eee1939c8a832
NurjahonErgashevMe <nurjahonergashevme@gmail.com>
2025-05-04 02:57:05 +0500

    rollback testing

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,7 +11,7 @@ class Settings:
     EVIRMA_JSON_PATH = os.path.join(OUTPUT_DIR, "evirma.json")
     WB_CATALOG_URL = "https://static-basket-01.wbbasket.ru/vol0/data/main-menu-ru-ru-v3.json"
     EVIRMA_API_URL = "https://evirma.ru/api/v1/keyword/list"
-    MAX_PAGES = 50
+    MAX_PAGES = 2
     PRODUCTS_PER_PAGE = 100
     FILE_DELETE_DELAY = 15  # Секунды
 
